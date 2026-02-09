0a43b10d1722373923856848b79fb813ed553f97
ravipaswanjs-debug <ravipaswan.js@gmail.com>
2026-02-09 17:45:22 +0530

    Update stm32f411xx.h and added the required usart code in the device specific hedader file

--- inc/stm32f411xx.h
+++ inc/stm32f411xx.h
@@ -166,6 +166,16 @@ typedef struct{
 	__vo uint32_t TRISE;
 	__vo unit32_t FLTR;
 }I2C_RegDef_t;
+
+typedef struct{
+	__vo uint32_t SR;
+	__vo uint32_t DR;
+	__vo uint32_t BRR;
+	__vo uint32_t CR1;
+	__vo uint32_t CR2;
+	__vo uint32_t CR3;
+	__vo uint32_t GTPR;
+}USART_RegDef_t;
 /*
  * Peripheral Definitions (Peripheral base addresses typecasted to xx_RegDef_t
  */
@@ -188,6 +198,14 @@ typedef struct{
 #define I2C1		((I2C_RegDef_t*)I2C1_BASEADDR)
 #define I2C2		((I2C_RegDef_t*)I2C2_BASEADDR)
 #define I2C3		((I2C_RegDef_t*)I2C3_BASEADDR)
+
+#define USART1		((USART_RegDef_t*)USART1_BASEADDR)
+#define USART2		((USART_RegDef_t*)USART2_BASEADDR)
+#define USART6		((USART_RegDef_t*)USART6_BASEADDR)
+/*
+*. Periphheral address for the USART Peripherela
+*/
+
 
 /*
  * Clock Enable Macros for GPIOx Peripherals
@@ -305,6 +323,15 @@ typedef struct{
 #define I2C2_REG_RESET()				do{(RCC->APB1RSTR |= (1 << 22)); (RCC->APB1RSTR &= ~(1 << 22));}while(0)
 #define I2C1_REG_RESET()				do{(RCC->APB1RSTR |= (1 << 23)); (RCC->APB1RSTR &= ~(1 << 23));}while(0)
 
+/*
+* USART Register Reset Macros
+*/
+#define USART1_REG_RESET()				do{(RCC->APB2RSTR |= (1 << 4)); (RCC->APB2RSTR &= ~(1 << 4));}while(0)
+#define USART2_REG_RESET()				do{(RCC->APB1RSTR |= (1 << 17)); (RCC->APB1RSTR &= ~(1 << 17));}while(0)
+#define USART6_REG_RESET()				do{(RCC->APB1RSTR |= (1 << 5)); (RCC->APB1RSTR &= ~(1 << 5));}while(0)
+
+
+
 // Some Generic Macros
 #define ENABLE 				1
 #define DISABLE 			0
@@ -376,9 +403,81 @@ typedef struct{
 #define I2C_CCR_DUTY 					14
 #define I2C_CCR_FS  				 	15
 
+/***********************************************************************
+ *					Bit Position Definitions of USART Registers
+ **********************************************************************/
+
+/*
+ * Bit position definitions USART_SR (Status Register)
+ */
+#define USART_SR_PE                        0
+#define USART_SR_FE                        1
+#define USART_SR_NF                        2
+#define USART_SR_ORE                       3
+#define USART_SR_IDLE                      4
+#define USART_SR_RXNE                      5
+#define USART_SR_TC                        6
+#define USART_SR_TXE                       7
+#define USART_SR_LBD                       8
+#define USART_SR_CTS                       9
+
+/*
+ * Bit position definitions USART_BRR (Baud Rate Register)
+ */
+#define USART_BRR_DIV_FRACTION             0
+#define USART_BRR_DIV_MANTISSA             4
+
+/*
+ * Bit position definitions USART_CR1 (Control Register 1)
+ */
+#define USART_CR1_SBK                      0
+#define USART_CR1_RWU                      1
+#define USART_CR1_RE                       2
+#define USART_CR1_TE                       3
+#define USART_CR1_IDLEIE                   4
+#define USART_CR1_RXNEIE                   5
+#define USART_CR1_TCIE                     6
+#define USART_CR1_TXEIE                    7
+#define USART_CR1_PEIE                     8
+#define USART_CR1_PS                       9
+#define USART_CR1_PCE                      10
+#define USART_CR1_WAKE                     11
+#define USART_CR1_M                        12
+#define USART_CR1_UE                       13
+#define USART_CR1_OVER8                    15
+
+/*
+ * Bit position definitions USART_CR2 (Control Register 2)
+ */
+#define USART_CR2_ADD                      0
+#define USART_CR2_LBDL                     5
+#define USART_CR2_LBDIE                    6
+#define USART_CR2_LBCL                     8
+#define USART_CR2_CPHA                     9
+#define USART_CR2_CPOL                     10
+#define USART_CR2_CLKEN                    11
+#define USART_CR2_STOP                     12
+#define USART_CR2_LINEN                    14
+
+/*
+ * Bit position definitions USART_CR3 (Control Register 3)
+ */
+#define USART_CR3_EIE                      0
+#define USART_CR3_IREN                     1
+#define USART_CR3_IRLP                     2
+#define USART_CR3_HDSEL                    3
+#define USART_CR3_NACK                     4
+#define USART_CR3_SCEN                     5
+#define USART_CR3_DMAR                     6
+#define USART_CR3_DMAT                     7
+#define USART_CR3_RTSE                     8
+#define USART_CR3_CTSE                     9
+#define USART_CR3_CTSIE                    10
+#define USART_CR3_ONEBIT                   11
+
 #include "stm32f411xx_gpio_driver.h"
 #include "stm32f411xx_spi_driver.h"
 #include "stm32f411xx_i2c_driver.h"
-
+#include "stm32f411xx_usart_driver.h"
 
 #endif /* INC_STM32F411XX_H_ */
